cc3f8ef98d3ea058a4e5019ce0fae787b06c8e54
Tim Kruse <coding@tim-kruse.com>
2024-02-28 11:33:11 +0100

    hover bugfix

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -14,7 +14,7 @@ export default function SidebarItem({icon, text, active, alert}: SidebarItemProp
         <li className={`
             relative flex items-center py-2 px-3 my-1
             font-medium rounded-md cursor-pointer
-            transition-colors
+            transition-colors group
             ${
                 active
                     ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
