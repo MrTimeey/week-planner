9c73568af8766eb154c4e32cb09c21e96f9ef34a
Tim Kruse <coding@tim-kruse.com>
2024-02-28 10:55:59 +0100

    More sidebar stuff

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,8 @@
 import './layout.css'
 import React from 'react';
 import Sidebar from "./Sidebar.tsx";
+import SidebarItem from "./SidebarItem.tsx";
+import {BarChart3, Boxes, LayoutDashboard, LifeBuoy, Package, Receipt, Settings, UserCircle} from "lucide-react";
 
 type LayoutProps = {
     children: string | React.JSX.Element | React.JSX.Element[]
@@ -10,7 +12,17 @@ export default function Layout({children}: LayoutProps) {
     return (
         <div className="layout">
             <div className="sidebar">
-                <Sidebar children={[]}/>
+                <Sidebar>
+                    <SidebarItem icon={<LayoutDashboard size={20}/> } text="Dashboard" alert />
+                    <SidebarItem icon={<BarChart3 size={20}/> } text="Statistics" active />
+                    <SidebarItem icon={<UserCircle size={20}/> } text="Users" />
+                    <SidebarItem icon={<Boxes size={20}/> } text="Inventory" />
+                    <SidebarItem icon={<Package size={20}/> } text="Orders" alert />
+                    <SidebarItem icon={<Receipt size={20}/> } text="Billings" />
+                    <hr className="my-3" />
+                    <SidebarItem icon={<Settings size={20}/> } text="Settings" />
+                    <SidebarItem icon={<LifeBuoy size={20}/> } text="Help" />
+                </Sidebar>
             </div>
             <div className="header" style={{ background: "darkblue"}}>
                 <p>Header</p>
